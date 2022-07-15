45d8011896c084adde87f506e3b4b136f67b0b64
Jerson Pool Miranda Robles <jersoersonpool.mirandarobles@nttdata.com>
2022-07-15 11:59:04 -0500

    fix: resolve comments

--- routes/identifiers.ts
+++ routes/identifiers.ts
@@ -17,6 +17,7 @@ class Identifiers {
         abiIM: JSON.parse(fs.readFileSync(process.cwd()+"/abiIM.json", 'utf8')),
         abiProxy: JSON.parse(fs.readFileSync(process.cwd()+"/abiProxy.json", 'utf8')),
         addressIM: process.env.ADDRESS_IM,
+        headers: []
     };
 
     constructor() {
@@ -26,22 +27,20 @@ class Identifiers {
 
     private routes(): void {
 
-        this.express.get("/identifiers/:did", async (req, res, next) => {
+        this.express.get("/identifiers/:identifier", async (req, res, next) => {
             const { params } = req;
-            if (!params?.did) {
+            if (!params?.identifier) {
                 res.status(400).send("Params not contains did");
                 return;
             }
-            const headers = [{ name: 'Authorization', value: req.headers.authorization }];
-            const newOptions = { ...this.options, headers: headers, ...req.body };
-            const evResolver = EvResolver.getResolver(newOptions);
+            const evResolver = EvResolver.getResolver(this.options);
             const resolver = new Resolver(evResolver);
             try {
-                const didDocument = await resolver.resolve(params.did);
-                logger.info(`Resolve did: ${params.did} - ${JSON.stringify(didDocument)}`);
+                const didDocument = await resolver.resolve(params.identifier);
+                logger.info(`Resolve did: ${params.identifier} - ${JSON.stringify(didDocument)}`);
                 res.status(200).json(didDocument);
             } catch (error: any) {
-                const responseMessage = `Can't resolve did: ${params.did}`;
+                const responseMessage = `Can't resolve did: ${params.identifier}`;
                 logger.error(`${responseMessage} - ${error.message}`);
                 res.status(500).json(responseMessage);
             }
